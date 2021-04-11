77741743e362452df6b80ec7bab7014ee59fe3f2
z5160618 <z5160618@unsw.edu.au>
2021-04-11 17:59:00 +1000

    starting dashboard

---
+++ frontend/src/components/GameTile.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+// import { Link } from 'react-router-dom';
+// import Navbar from 'react-bootstrap/Navbar';
+// import Nav from 'react-bootstrap/Nav';
+
+const GameTile = () => {
+  return (
+  <div className="GameTile">
+    This is a game tile lmao
+  </div>
+  );
+}
+
+export default GameTile;
--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -1,11 +1,33 @@
 import React from 'react';
 import Header from '../components/Header.jsx';
+import GameTile from '../components/GameTile.jsx';
+import axios from 'axios';
 
 const Dashboard = () => {
+  const getGames = async () => {
+    const token = localStorage.getItem('token');
+    const response = await axios.get('http://localhost:5005/admin/quiz', {
+      token
+    }, {
+      headers: {
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      }
+    }).catch(e => console.log(e.response.data.error));
+    if (response !== undefined && response.status === 200) {
+      return response.data.quizzes;
+    }
+  };
+
+  getGames.then(console.log);
+
   return (
     <div>
       <Header />
       <h2>Dashboard</h2>
+      <div className="GameTileContainer">
+        <GameTile />
+      </div>
     </div>
   );
 }
