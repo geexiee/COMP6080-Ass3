f45b7f2a2c4e01b4cd9d9903f2d7fa0b70c908f7
dtao <dtao45@gmail.com>
2021-04-12 14:23:03 +1000

    clean up dashboard and put on hold

--- frontend/src/pages/Dashboard.jsx
+++ frontend/src/pages/Dashboard.jsx
@@ -13,12 +13,13 @@ const Dashboard = () => {
     }).catch(e => console.log(e.response.data.error));
     if (response !== undefined && response.status === 200) {
       console.log(response.data.quizzes);
-      const anchor = document.getElementById('quiz');
+      // const anchor = document.getElementById('quiz');
       (response.data.quizzes).forEach(quiz => {
         console.log(quiz.id);
+        <div key={quiz.id}>{quiz.id}</div>
         const tile = React.createElement('Card', { style: { className: 'card' } }, quiz.id);
-        const container = React.createElement('div', { style: { 'background-color': 'yellow' } }, tile);
-        anchor.appendChild(tile);
+        const container = React.createElement('div', { style: { backgroundColor: 'yellow' } }, tile);
+        // anchor.appendChild(tile);
         // want to find a way to append all existing quizzes on diff cards
         // this currently will replace whatever was previously there
         ReactDOM.render(container, document.getElementById('quiz'));
