bd085fc5b7d86b77e05d6a51bb53bbfd36ddf5e1
Gordon Xie <z5160618@student.unsw.edu.au>
2021-04-19 17:35:01 +1000

    started testing

---
+++ frontend/src/tests/GameTile.test.js
@@ -0,0 +1,35 @@
+import React from 'react';
+// import { render, screen } from '@testing-library/react';
+import GameTile from '../components/GameTile';
+import { shallow } from 'enzyme';
+
+// test('GameTile has edit game button', () => {
+//   render(<GameTile />);
+//   const linkElement = screen.getByText(/home/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+
+describe('GameTile testing', () => {
+
+  it('has a specified game name', () => {
+    const gameName = 'testname'
+    const gameTile = shallow(<GameTile ID={0} owner={'testOwner'} name={gameName}
+       img='https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png'/>);
+    expect(gameTile.text()).toContain(gameName);
+  });
+
+  it('has a specified game owner', () => {
+    const gameOwner = 'testowner'
+    const gameTile = shallow(<GameTile ID={0} owner={gameOwner} name={'testnName'}
+       img='https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png'/>);
+    expect(gameTile.text()).toContain(gameOwner);
+  });
+
+  it('triggers setGoEditGame when editGame button has been clicked', () => {
+    const gameOwner = 'testowner'
+    const gameTile = shallow(<GameTile ID={0} owner={gameOwner} name={'testnName'}
+       img='https://media.sproutsocial.com/uploads/2017/02/10x-featured-social-media-image-size.png'/>);
+    expect(gameTile.text()).toContain(gameOwner);
+  });
+
+});
