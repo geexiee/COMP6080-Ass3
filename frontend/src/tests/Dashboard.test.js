9e6e6c0b0cd80b501b03139c91056de637d19ada
dtao <dtao45@gmail.com>
2021-04-23 02:32:39 +1000

    testing comments and adding to .md files

--- frontend/src/tests/Dashboard.test.js
+++ frontend/src/tests/Dashboard.test.js
@@ -6,6 +6,7 @@ import Button from '@material-ui/core/Button';
 describe('Dashboard testing', () => {
   it('has an "add new game" button', () => {
     const dashboard = shallow(<Dashboard/>);
+    // check create new game button exists on dashboard
     expect(dashboard.find(Button).text()).toEqual('Create New Game');
   });
 });
--- frontend/src/tests/GameTile.test.js
+++ frontend/src/tests/GameTile.test.js
@@ -12,33 +12,69 @@ const testQuizDetails = {
 
 describe('GameTile testing', () => {
   it('has a specified game name', () => {
-    const card = shallow(<GameTile ID={testQuizDetails.ID} name={testQuizDetails.name}
-       owner={testQuizDetails.owner} img={testQuizDetails.img} />);
+    const card = shallow(
+    <GameTile
+      ID={testQuizDetails.ID}
+      name={testQuizDetails.name}
+      owner={testQuizDetails.owner}
+      img={testQuizDetails.img}
+    />
+  );
+    // check the game name matches the prop
     expect(card.find('#gameName').prop('children')).toContain(testQuizDetails.name);
   });
 
   it('has a specified game owner', () => {
-    const card = shallow(<GameTile ID={testQuizDetails.ID} name={testQuizDetails.name}
-      owner={testQuizDetails.owner} img={testQuizDetails.img} />);
+    const card = shallow(
+      <GameTile
+        ID={testQuizDetails.ID}
+        name={testQuizDetails.name}
+        owner={testQuizDetails.owner}
+        img={testQuizDetails.img}
+      />
+    );
+    // check game owner is correct
     expect(card.find('#gameOwner').prop('children')).toContain(testQuizDetails.owner);
   });
 
   it('has a button to edit quiz', () => {
-    const card = shallow(<GameTile ID={testQuizDetails.ID} name={testQuizDetails.name}
-      owner={testQuizDetails.owner} img={testQuizDetails.img} />);
+    const card = shallow(
+      <GameTile
+        ID={testQuizDetails.ID}
+        name={testQuizDetails.name}
+        owner={testQuizDetails.owner}
+        img={testQuizDetails.img}
+      />
+    );
+    // check there exists an edit game button with corresponding text
     expect(card.find(Button).find('.editGameButton').prop('children')).toContain('Edit Game');
   });
 
   it('has a button to delete quiz', () => {
-    const card = shallow(<GameTile ID={testQuizDetails.ID} name={testQuizDetails.name}
-      owner={testQuizDetails.owner} img={testQuizDetails.img} />);
+    const card = shallow(
+      <GameTile
+        ID={testQuizDetails.ID}
+        name={testQuizDetails.name}
+        owner={testQuizDetails.owner}
+        img={testQuizDetails.img}
+      />
+    );
+    // check there exists an delete game button with corresponding text
     expect(card.find(Button).find('.deleteGameButton').prop('children')).toContain('Delete Game');
   });
 
   it('opens the game started modal when clicked', () => {
-    const card = shallow(<GameTile ID={testQuizDetails.ID} name={testQuizDetails.name}
-      owner={testQuizDetails.owner} img={testQuizDetails.img } />);
+    const card = shallow(
+      <GameTile
+        ID={testQuizDetails.ID}
+        name={testQuizDetails.name}
+        owner={testQuizDetails.owner}
+        img={testQuizDetails.img}
+      />
+    );
+    // click start game button
     card.find('#startGameButton').simulate('click');
+    // check that the game start modal appears
     expect(card.find('#gameModalTitle').prop('children')).toContain('Game Started');
   });
 });
